173a8e624fffd4a6c37bde1e5f073ed8fc37ff43
nrmnqdds <quddusnuriman@gmail.com>
2024-01-07 15:40:37 +0800

    :lipstick: creates custom default neobrutalism components

---
+++ components/default/button.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/common/cn";
+import * as React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <button
+        type="button"
+        className={cn(
+          "text-black border-black border-2 p-2.5 bg-purple-300 hover:bg-purple-400 hover:shadow-default transition-all active:bg-purple-500 rounded-md",
+          className
+        )}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button };
