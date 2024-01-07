e59dd15ce54f7ade75d238afde700dd209eb6bfd
nrmnqdds <quddusnuriman@gmail.com>
2024-01-07 15:44:37 +0800

    :wheelchair: session hooks

---
+++ hooks/session-store.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+type sessionProps = {
+  id?: string;
+  name: string;
+  image: string;
+  matricNo: string;
+  points: number;
+  sessionToken?: string | null;
+};
+
+type SessionStore = {
+  session: sessionProps | null | undefined;
+  setSession: (session: sessionProps | null | undefined) => void;
+};
+
+export const useSessionStore = create(
+  persist<SessionStore>(
+    (set) => ({
+      session: null,
+      setSession: (session) => set({ session }),
+    }),
+    {
+      name: "session-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
