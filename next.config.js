b30dcaccc1514354461620d34f8f679a2d721010
nrmnqdds <quddusnuriman@gmail.com>
2024-01-16 22:32:35 +0800

    :wrench: configure image optimization for s3

--- next.config.js
+++ next.config.js
@@ -16,6 +16,12 @@ const nextConfig = {
         pathname: "**",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "istudy-bucket2.s3.ap-southeast-1.amazonaws.com",
+        pathname: "**",
+        port: "",
+      },
     ],
   },
 };
